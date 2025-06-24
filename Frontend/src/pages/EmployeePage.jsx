d2e430d7efa46c5c76a3c872adca3632b1166c91
Manju Vallabha Pavalla <143684911+Manju-Vallabha@users.noreply.github.com>
2025-06-24 15:43:19 +0530

    Update EmployeePage.jsx

--- Frontend/src/pages/EmployeePage.jsx
+++ Frontend/src/pages/EmployeePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { FaEdit, FaCheckCircle } from "react-icons/fa";
 import { FaRegCommentDots } from "react-icons/fa";
