b2c1f43159f33d6e9bdd1ad5f87a1d5ccee73848
Manju Vallabha Pavalla <143684911+Manju-Vallabha@users.noreply.github.com>
2025-06-24 15:42:25 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ✅ Import this
 import { ToastContainer, Zoom } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App.jsx";
